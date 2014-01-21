aa998bc3a10a56080b8a243694b9c1763d300fb3
anthony-economides-GL <anthony.economides@gmail.com>
2014-01-21 19:48:06 +0100

    fix ortho

--- src/realmatrixjava/RealMatrixJava.java
+++ src/realmatrixjava/RealMatrixJava.java
@@ -5,7 +5,7 @@ import org.apache.commons.math3.linear.RealMatrix;
 
 public class RealMatrixJava {
 	
-	//attention au floats : 3.5 et pas 3,5
+	//attention aux floats : 3.5 et pas 3,5
 	static int u=1;
 	static double[] P0= {6 , 6 , 6 };	//valeurs ajouté au pif
 	static double[] P1= {4 , -6 , 2.5}; //valeurs ajouté au pif
@@ -16,22 +16,22 @@ public class RealMatrixJava {
 	
 	public static void main(String[] args) {
 	
-		//tableux 1
+		//tableaux 1
 		double[][] matrixData = { {u^3, u^2, u^1, 0} };
 		RealMatrix n = MatrixUtils.createRealMatrix(matrixData);
 		
-		//tableux 2
+		//tableaux 2
 		double[][] matrixData2 = { {2,-2,1,1}, {-3,3,-2,-1}, {0,0,1,0}, {1,0,0,0} };
 		RealMatrix m = MatrixUtils.createRealMatrix(matrixData2);
 		
-		//tableux 3
+		//tableaux 3
 		double[][] matrixData3 = { P0, P1, D0, D1 };
 		RealMatrix k = MatrixUtils.createRealMatrix(matrixData3);
 		//multiplication No1
 		
 		RealMatrix multip1 = n.multiply(m);
 		
-		//imprimer le tableaux resultat de la premiere multiplication
+		//imprimer le tableaux résultat de la première multiplication
 		
 		printMatrix(multip1);
 		System.out.println("---------------------");
@@ -39,7 +39,7 @@ public class RealMatrixJava {
 		//multiplication No2
 		RealMatrix Resultat=multip1.multiply(k);
 		
-		//imprimer le tableaux resultat de la deuxieme multiplication
+		//imprimer le tableaux résultat de la deuxième multiplication
 		printMatrix(Resultat);
 	}
 	
